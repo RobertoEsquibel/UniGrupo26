2005d8aed0dd76c2004007cede92f3f33c78f7b8
RobertoEsquibel <cristianesquibel@abc.gob.ar>
2023-09-12 21:39:37 -0300

    volvi a subir el tostring

--- src/universidadgrupo26/entidades/Alumno.java
+++ src/universidadgrupo26/entidades/Alumno.java
@@ -86,6 +86,11 @@ public class Alumno {
     public void setActivo(boolean activo) {
         this.activo = activo;
     }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + '}';
+    }
     
     
 }
